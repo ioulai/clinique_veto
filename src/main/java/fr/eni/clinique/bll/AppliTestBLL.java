ff662684953038df4c1d4822caa77744731f24c5
ioulai <oulaiisaac@gmail.com>
2018-02-26 14:50:07 +0100

    test

---
+++ src/main/java/fr/eni/clinique/bll/AppliTestBLL.java
@@ -0,0 +1,9 @@
+package fr.eni.clinique.bll;
+
+public class AppliTestBLL {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/fr/eni/clinique/bll/exception/BLLException.java
@@ -0,0 +1,5 @@
+package fr.eni.clinique.bll.exception;
+
+public class BLLException {
+
+}
